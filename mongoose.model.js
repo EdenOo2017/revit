6e320a72dabbeb3c64b7b146043609d6778c9768
Mother <thannaingoo.api@gmail.com>
2018-01-24 13:05:47 +0800

    Initial Commit

---
+++ mongoose.model.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var url = "mongodb://admin:Utno1985!@ds135757.mlab.com:35757/funding"
+
+mongoose.connect(url, { useMongoClient: true });
+mongoose.Promise = global.Promise;
+var Schema = mongoose.Schema;
+
+var userDataSchema = new Schema({
+    Block: String,
+ 
+}, { collection: 'revit' });
+
+var UserData = mongoose.model('UserData', userDataSchema);
+
+module.exports = UserData;
+
+
---
+++ serverserver.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var cors = require('cors')
+var bodyParser = require('body-parser');
+var UserData = require('./mongoose.model');
+var app = express();
+const server = require('http').createServer(app)
+const io = require('socket.io').listen(server);
+const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(bodyParser.json());
+
+app.set('port', port);
+app.use('/', express.static(__dirname + '/www'));
+
+server.listen(port, () => {
+  console.log('Server is up');
+});
+
+function calcTime() {
+  var d = new Date();
+  var utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+  var nd = new Date(utc + (3600000 * 8));
+  var updateDate = nd.toLocaleDateString() + "T" + (nd.toLocaleTimeString());
+  return updateDate;
+}
+
+//#region API......................................................
+
+app.get('/getBlock', (req, res) => {
+  UserData.find({}).then(function (doc) {
+    res.send(doc.map(document => document.Block));
+  });
+});
+
+//#endregion
+
+module.exports = { app };
