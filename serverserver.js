ee87988bf78e11612594030cc85e84371803f112
Mother <thannaingoo.api@gmail.com>
2018-01-25 16:01:51 +0800

    Initial Commit

--- serverserver.js
+++ serverserver.js
@@ -29,7 +29,7 @@ function calcTime() {
 app.get('/getBlock', (req, res) => {
   UserData.find({},{ _id: 0}).then(function (doc) {
     //res.send(doc.map(document => document._id));
-    res.send(doc.Block-1);
+    res.send(doc.keys(1));
   });
 });
 
