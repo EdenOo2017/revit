7b363faf46f81f45c41c8728220e4d9b5fbac468
Mother <thannaingoo.api@gmail.com>
2018-01-26 09:24:29 +0800

    Initial Commit

--- serverserver.js
+++ serverserver.js
@@ -32,7 +32,7 @@ app.get('/getBlock', (req, res) => {
       
     //var keys = Object.keys(doc[0].BLOCK[0]);   
 
-   res.send(doc[0].BLOCK[0].Block-1[0]);
+   res.send(doc[0].BLOCK[0].Block-1);
   });
 });
 
