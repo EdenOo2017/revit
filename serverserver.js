5cf9585ff68a23054d44c010bab50e8ca1117da3
Mother <thannaingoo.api@gmail.com>
2018-01-26 09:34:12 +0800

    Initial Commit

--- serverserver.js
+++ serverserver.js
@@ -30,7 +30,7 @@ app.get('/getBlock', (req, res) => {
   UserData.find({},{ _id: 0}).then(function (doc) {
    // res.send(doc.map(document => document.BLOCK));      
     
-   res.send(doc[0].BLOCK[0]);
+   res.send(doc[0].BLOCK[0][0]);
   });
 });
 
